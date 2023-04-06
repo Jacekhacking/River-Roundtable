c23acdbf177ad2eb62c2b47561d70480fb8a2121
JacekHacking <Jacekhacking@mac.com>
2023-04-06 12:10:12 -0600

    changed db url to be safe

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString:
-    'postgresql://postgres:uEfAxLDJKQgEy6KuhcdF@containers-us-west-56.railway.app:6199/railway',
+  connectionString: process.env.DATABASE_URL,
 });
 export default pool;
